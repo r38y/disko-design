183842172933c3d5f49bf6fd5f7d144b6b28a224
Randy Schmidt <randy@forge38.com>
2012-12-14 16:24:33 -0500

    Initialize the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiskoDesign::Application.config.secret_token = '2bca02b5b41ac34a71745fa907e7c37c7f637d67349baca0711acfbe7f907a486bdaf707736938fd45ce94054e0bf0f645bcc4b9648bf437b675231a300b3fce'
